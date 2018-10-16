4cfe7c3ed4758b6a1fea721bd60133c86d3f5d1d
Cody Riewerts <carc35@mail.umsl.edu>
2018-10-15 22:13:03 -0500

    final version

---
+++ shoppingList/script.js
@@ -0,0 +1,14 @@
+var myApp = angular.module("myList", []);
+myApp.controller("myListController", function($scope) {
+    $scope.items = ["Butter", "Flour", "Milk"];
+    $scope.newItem = "";
+    $scope.pushItem = function () {
+        if($scope.newItem != "") {
+            $scope.items.push($scope.newItem);
+            $scope.newItem = "";
+        }
+    }
+    $scope.deleteItem = function(index) {
+        $scope.items.splice(index, 1);
+    }
+});
